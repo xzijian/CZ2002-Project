c98d27758365a8e1778d5e874efb561eda80a65a
hermeess <90948009+hermeess@users.noreply.github.com>
2021-10-23 22:29:41 +0800

    Customer Textfile + ArrayList

--- src/CustomerDB.java
+++ src/CustomerDB.java
@@ -1,30 +1,43 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
 import java.io.FileWriter;
 import java.util.Scanner;
+import java.util.*;
+
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class CustomerDB {
+	private static final String filepath= "TextFolder/customerDB.txt";
+	
 	public void createCustomerDB() {
 		try {
-			File myObj = new File("C:\\Users\\herme\\Dropbox\\My PC (LAPTOP-T0F2VDR7)\\Desktop\\NTUY2S1\\CZ2002\\Project\\proj\\src\\customerDB.txt");
+			File myObj = new File("TextFolder/customerDB.txt");
 			if (myObj.createNewFile()) {
 				System.out.println("File created: " + myObj.getName());
 			} else {
 				System.out.println("File already exists.");
-			}
+			}	
 		} catch (IOException e) {
 			System.out.println("An Error occured.");
 			e.printStackTrace();
 		}
 	}
-	public void updateCustomerDB(Customer c) {
+
+	
+	public void updateCustomerDB(Customer c) throws ClassNotFoundException {
 		int lineNumber = 0;
 		int exist = 0;
 		try {
-			FileWriter myWriter = new FileWriter("C:\\\\Users\\\\herme\\\\Dropbox\\\\My PC (LAPTOP-T0F2VDR7)\\\\Desktop\\\\NTUY2S1\\\\CZ2002\\\\Project\\\\proj\\\\src\\\\customerDB.txt", true);
+			FileWriter myWriter = new FileWriter("TextFolder/customerDB.txtt", true);
 			if (exist == 0) {
 				try {
-					File file = new File("C:\\\\Users\\\\herme\\\\Dropbox\\\\My PC (LAPTOP-T0F2VDR7)\\\\Desktop\\\\NTUY2S1\\\\CZ2002\\\\Project\\\\proj\\\\src\\\\customerDB.txt");
+					File file = new File("TextFolder/customerDB.txt");
 					Scanner sc = new Scanner(file);
 					
 					while(sc.hasNextLine()) {
@@ -41,10 +54,10 @@ public class CustomerDB {
 					}
 			}
 			if (exist == 0) {
-				myWriter.write("\n{\n\tCustomer Name: " + c.getCustomerName() + ","
-						+ "\n\tCustomer Contact: " + c.getCustomerContact() + "," 
-						+ "\n\tCustomer Membership: " + c.membershipStatus() + ","
-							+"\n}");
+				myWriter.write(c.getCustomerName() + "-"
+						+ c.getCustomerContact() + "-" 
+						+ c.membershipStatus() + "\n");
+				
 				myWriter.close();
 				System.out.println("Successfully added to CustomerDB.");
 			}
@@ -53,4 +66,29 @@ public class CustomerDB {
 			e.printStackTrace();
 		}
 	}
+
+	public ArrayList<Customer> returnArrayListOfCustomer() throws FileNotFoundException {
+		try {
+	    	List<String> customerLines = Files.readAllLines(java.nio.file.Paths.get("TextFolder/customerDB.txt"));
+	    	
+	    	for (String line: customerLines) {
+	    		Customer customer = new Customer1();
+	    		String[] tokens = line.split("-");
+	    		customer.setCustomerName(tokens[0]);
+	    		customer.setContact(tokens[1]);
+	    		if(tokens[2].toLowerCase() == "member") {
+	    			customer.setMembership();
+	    		}
+	    		else {
+	    			customer.cancelMembership();
+	    		}
+	    		cusArrList.add(customer);
+	    	}
+	    } catch (IOException e) {
+            e.printStackTrace();
+	    
+	    }	
+		return cusArrList;
+	}
+
 }
