49423cfd74b130105ce0b82bd8546d07de752e52
hermeess <90948009+hermeess@users.noreply.github.com>
2021-11-10 00:21:52 +0800

    addition of remove customer method

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -162,4 +162,22 @@ public class Restaurant {
 		System.out.println("Customer does not exist");
 		return null;		
 	}
+	//remove customer from the customerArrayList.
+	public static void removeCustomer(String number) {
+		int i = 0;
+		int removeC = 0;
+		for (Customer customer : Restaurant.customersList) {
+			if (customer.getCustomerContact().equals(number))
+			{
+				Restaurant.customersList.remove(i);
+				System.out.println("Customer: " + customer.getCustomerName() + " is removed from database.");
+				removeC = 1;
+				break;
+			}
+			i++;
+		}
+		if (removeC == 0) {
+			System.out.println("Customer not found.");
+		}
+	}
 }	
