03049f89d2e00409644a8ae03b783de2ed8bb0b8
hermeess <90948009+hermeess@users.noreply.github.com>
2021-11-10 12:46:34 +0800

    Customer Manager.
    Includes checkCustomer (no duplicate customer in database), returnCustomer (return customer object via phone number), removeCustomer (remove customer if their phone number is given)

---
+++ src/CustomerMgr.java
@@ -0,0 +1,49 @@
+public class CustomerMgr {
+	
+	//check if a customer exist via their phone number. Ensure no duplicate.
+	public static int checkCustomer(String name, String number, boolean member) {
+		int exist = 0;
+		if (Restaurant.customersList == null)
+			System.out.println("I am empty");
+		for (Customer customer : Restaurant.customersList) {
+			if (number.equals(customer.getCustomerContact()))
+			{
+				System.out.println("Customer exists");
+				exist = 1;
+				return exist;
+			}
+		}
+		return exist;
+	}
+	//obtain a customer via their number.
+	public static Customer getCustomer(String number) {
+		for (Customer customer :  Restaurant.customersList) {
+			if (customer.getCustomerContact().equals(number))
+			{
+				System.out.println("Customer found.");
+				return customer;
+			}
+					
+		}
+		System.out.println("Customer does not exist");
+		return null;		
+	}
+	//remove customer from the customerArrayList.
+	public static void removeCustomer(String number) {
+		int i = 0;
+		int removeC = 0;
+		for (Customer customer : Restaurant.customersList) {
+			if (customer.getCustomerContact().equals(number))
+			{
+				Restaurant.customersList.remove(i);
+				System.out.println("Customer: " + customer.getCustomerName() + " is removed from database.");
+				removeC = 1;
+				break;
+			}
+			i++;
+		}
+		if (removeC == 0) {
+			System.out.println("Customer not found.");
+		}
+	}
+}
